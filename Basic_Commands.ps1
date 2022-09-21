014e3c45a1af1534a09836012ec43fe5ef94adf0
prasanth <Prasanthmohanraj08@gmail.com>
2022-09-21 11:48:24 -0700

    Basic powershell kill commands

---
+++ Basic_Commands.ps1
@@ -0,0 +1,13 @@
+﻿#killing the Application commands
+
+Get-Process *chrome* | Stop-Process -Force
+
+Get-Process *msedge* | Stop-Process -Force
+
+Get-Process *OUTLOOK* | Stop-Process -Force
+
+Get-Process *EXCEL* | Stop-Process -Force
+
+Get-Process *WINWORD* | Stop-Process -Force
+
+
